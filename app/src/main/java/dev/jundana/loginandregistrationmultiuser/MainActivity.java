8e56639e116caabc3e99c15184cdd0573f7a032d
jundanaalbasyir <jalbasyir@gmail.com>
2020-09-07 21:19:02 +0700

    Project Starter

---
+++ app/src/main/java/dev/jundana/loginandregistrationmultiuser/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.jundana.loginandregistrationmultiuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
