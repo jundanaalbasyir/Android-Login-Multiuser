cbafa1c86afead423035c5a1b48bad2830971c81
jundanaalbasyir <jalbasyir@gmail.com>
2020-09-08 11:36:47 +0700

    craete user model

--- app/src/main/java/dev/jundana/loginandregistrationmultiuser/model/Users.java
+++ app/src/main/java/dev/jundana/loginandregistrationmultiuser/model/Users.java
@@ -1,4 +1,29 @@
 package dev.jundana.loginandregistrationmultiuser.model;
 
 public class Users {
+    private String email, username, password, status;
+    private int id;
+
+    public Users(int id, String username, String email, String status) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
