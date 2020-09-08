a339dce393b165ef158822e36f0f680a8fc0351c
jundanaalbasyir <jalbasyir@gmail.com>
2020-09-08 11:39:15 +0700

    helper url & request handler

--- app/src/main/java/dev/jundana/loginandregistrationmultiuser/helper/Url.java
+++ app/src/main/java/dev/jundana/loginandregistrationmultiuser/helper/Url.java
@@ -1,4 +1,7 @@
 package dev.jundana.loginandregistrationmultiuser.helper;
 
 public class Url {
+    private static final String URL_ROOT = "http://192.168.1.14/api/api.php?action=";
+    public static final String URL_REGISTER = URL_ROOT + "signup";
+    public static final String URL_LOGIN = URL_ROOT + "login";
 }
