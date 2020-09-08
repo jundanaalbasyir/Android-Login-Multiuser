4cfc17a7d1877910708db3c302318cd44c5f72a6
jundanaalbasyir <jalbasyir@gmail.com>
2020-09-08 13:16:09 +0700

    new ui admin and user

--- app/src/main/java/dev/jundana/loginandregistrationmultiuser/ui/user/UserActivity.java
+++ app/src/main/java/dev/jundana/loginandregistrationmultiuser/ui/user/UserActivity.java
@@ -1,13 +1,42 @@
 package dev.jundana.loginandregistrationmultiuser.ui.user;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import dev.jundana.loginandregistrationmultiuser.R;
+import dev.jundana.loginandregistrationmultiuser.helper.SharedPref;
+import dev.jundana.loginandregistrationmultiuser.model.Users;
 
 public class UserActivity extends AppCompatActivity {
+    TextView tvUsername, tvEmail, tvStatus;
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        tvUsername = findViewById(R.id.tvUsername);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvStatus = findViewById(R.id.tvStatus);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        Users user = SharedPref.getInstance(this).getUser();
+
+        tvUsername.setText(user.getUsername());
+        tvEmail.setText(user.getEmail());
+        tvStatus.setText(user.getStatus());
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPref.getInstance(getApplicationContext()).logout();
+                finish();
+            }
+        });
     }
 }
